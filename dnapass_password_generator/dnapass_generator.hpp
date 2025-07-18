d2364369a2762f506070e7b50ed0541e64d247df
Gerivan Costa <119078732+gerivanc@users.noreply.github.com>
2025-07-18 09:19:34 -0400

    Update dnapass_generator.hpp
    Update dnapass_generator.hpp

--- dnapass_password_generator/dnapass_generator.hpp
+++ dnapass_password_generator/dnapass_generator.hpp
@@ -1,11 +1,11 @@
-/* 
-* DNAPass Password Generator - Generate secure passwords inspired by DNA sequences 
-* Copyright © 2025 Gerivan Costa dos Santos 
-* Author: gerivanc 
-* GitHub: https://github.com/gerivanc/dnapass-password-generator 
-* MIT License: https://github.com/gerivanc/dnapass-password-generator/blob/main/LICENSE.md 
-*Version: 0.1.0 
-*/
+/*
+ * DNAPass Password Generator - Generate secure passwords inspired by DNA sequences
+ * Copyright © 2025 Gerivan Costa dos Santos
+ * Author: gerivanc
+ * GitHub: https://github.com/gerivanc/dnapass-password-generator
+ * MIT License: https://github.com/gerivanc/dnapass-password-generator/blob/main/LICENSE.md
+ * Version: 0.1.0
+ */
 
 #ifndef DNAPASS_GENERATOR_HPP
 #define DNAPASS_GENERATOR_HPP
@@ -16,18 +16,16 @@
 
 namespace dnapass {
 
-inline const std::string special_chars = "!@#$%^&*()_+-=[]{}|;:,.<>?~\\";
-
-struct PasswordResult { 
-std::string password; 
-std::vector<std::string> used_words; 
-std::vector<std::string> resolved_log;
+struct PasswordResult {
+    std::string password;
+    std::vector<std::string> used_words;
+    std::vector<std::string> resolved_log;
 };
 
-// Main function to generate passwords
+// Função principal para gerar senhas
 PasswordResult generate_password(int length, std::mt19937& rng);
 
-// Helper function to resolve ambiguous sequences
+// Função auxiliar para resolver sequências ambíguas
 std::pair<std::string, std::string> resolve_ambiguous_sequence(const std::string& sequence, std::mt19937& rng);
 
 } // namespace dnapass
